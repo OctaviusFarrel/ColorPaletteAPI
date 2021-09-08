20bfde7f12fefdc3a964ca58a6d73fb4fa0a9c7e
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2021-09-09 02:50:31 +0700

    Initial Commit

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const helpRouter = require('./routes/help');
-const getRouter = require('./routes/get');
 
 const app = express();
 
@@ -21,8 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/help',helpRouter);
-app.use('/get',getRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/help.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* Get HELP Page. */
-router.get('/', function(req, res, next) {
-  res.render('help');
-});
-
-module.exports = router;
