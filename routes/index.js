97734fa697cdb6242a829511500d0f1492cac65e
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2021-09-09 03:04:32 +0700

    feat: Adding local database (NOT RECOMMENDED!)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const data = require('./../public/json/data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send({});
+  res.send(data);
 });
 
 module.exports = router;
